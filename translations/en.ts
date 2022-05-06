820787ae1c6f5e91cb3d6c3f4d1f655c199e92f1
josedelinux <josedelinux@hotmail.com>
2022-05-06 22:18:03 +0800

    buggy update

--- translations/en.ts
+++ translations/en.ts
@@ -4,53 +4,113 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../../main.py" line="115"/>
+        <location filename="../main.py" line="115"/>
         <source>Car ++</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../main.py" line="133"/>
+        <location filename="../main.py" line="133"/>
         <source>Camera tab</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../main.py" line="134"/>
+        <location filename="../main.py" line="134"/>
         <source>Image tab</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../main.py" line="135"/>
+        <location filename="../main.py" line="135"/>
         <source>Log tab</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../main.py" line="144"/>
+        <location filename="../main.py" line="144"/>
         <source>Open Camera</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../main.py" line="146"/>
+        <location filename="../main.py" line="146"/>
         <source>Open camera</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../main.py" line="159"/>
+        <location filename="../main.py" line="159"/>
         <source>Detect Image</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../main.py" line="161"/>
+        <location filename="../main.py" line="161"/>
         <source>Detect image</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../main.py" line="200"/>
+        <location filename="../main.py" line="200"/>
         <source>Open a folder to images</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../main.py" line="201"/>
+        <location filename="../main.py" line="201"/>
         <source>Open an image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="195"/>
+        <source>Open &amp;Folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="198"/>
+        <source>Open &amp;Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="210"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="214"/>
+        <source>E&amp;xit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="216"/>
+        <source>&amp;Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="217"/>
+        <source>&amp;Preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="220"/>
+        <source>&amp;Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="225"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="226"/>
+        <source>&amp;Website</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="227"/>
+        <source>Show Help (&amp;Markdown)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="229"/>
+        <source>Show Help (HTML)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="231"/>
+        <source>About</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- translations/zh.ts
+++ translations/zh.ts
@@ -4,54 +4,114 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../../main.py" line="115"/>
+        <location filename="../main.py" line="115"/>
         <source>Car ++</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../main.py" line="133"/>
+        <location filename="../main.py" line="133"/>
         <source>Camera tab</source>
         <translation>相机页</translation>
     </message>
     <message>
-        <location filename="../../main.py" line="134"/>
+        <location filename="../main.py" line="134"/>
         <source>Image tab</source>
         <translation>相片页</translation>
     </message>
     <message>
-        <location filename="../../main.py" line="135"/>
+        <location filename="../main.py" line="135"/>
         <source>Log tab</source>
         <translation>日志页</translation>
     </message>
     <message>
-        <location filename="../../main.py" line="144"/>
+        <location filename="../main.py" line="144"/>
         <source>Open Camera</source>
         <translation>打开摄像头</translation>
     </message>
     <message>
-        <location filename="../../main.py" line="146"/>
+        <location filename="../main.py" line="146"/>
         <source>Open camera</source>
         <translation>打开摄像头</translation>
     </message>
     <message>
-        <location filename="../../main.py" line="159"/>
+        <location filename="../main.py" line="159"/>
         <source>Detect Image</source>
         <translation>检测图像</translation>
     </message>
     <message>
-        <location filename="../../main.py" line="161"/>
+        <location filename="../main.py" line="161"/>
         <source>Detect image</source>
         <translation>检测图像</translation>
     </message>
     <message>
-        <location filename="../../main.py" line="200"/>
+        <location filename="../main.py" line="200"/>
         <source>Open a folder to images</source>
         <translation>打开包含图片的文件夹</translation>
     </message>
     <message>
-        <location filename="../../main.py" line="201"/>
+        <location filename="../main.py" line="201"/>
         <source>Open an image</source>
         <translation>打开图片</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="195"/>
+        <source>Open &amp;Folder</source>
+        <translation>打开文件夹[&amp;F]</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="198"/>
+        <source>Open &amp;Image</source>
+        <translation>打开图像[I]</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="210"/>
+        <source>&amp;File</source>
+        <translation>文件[&amp;F]</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="214"/>
+        <source>E&amp;xit</source>
+        <translation>退出[&amp;x]</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="216"/>
+        <source>&amp;Options</source>
+        <translation>选项[&amp;O]</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="217"/>
+        <source>&amp;Preferences</source>
+        <translation>首选项[&amp;P]</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="220"/>
+        <source>&amp;Language</source>
+        <translation>语言[&amp;L]</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="225"/>
+        <source>&amp;Help</source>
+        <translation>帮助[&amp;H]</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="226"/>
+        <source>&amp;Website</source>
+        <translation>网站[&amp;W]</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="227"/>
+        <source>Show Help (&amp;Markdown)</source>
+        <translation>帮助文档[&amp;Markdown]</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="229"/>
+        <source>Show Help (HTML)</source>
+        <translation>帮助文档(HTML)</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="231"/>
+        <source>About</source>
+        <translation>帮助</translation>
     </message>
 </context>
 </TS>

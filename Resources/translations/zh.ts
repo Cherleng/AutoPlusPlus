68409eb74e32bfaf215b0f264a02c5cb0d6b4327
josedelinux <josedelinux@hotmail.com>
2022-05-06 17:19:18 +0800

    Update zh.ts

--- Resources/translations/zh.ts
+++ Resources/translations/zh.ts
@@ -1,57 +1,57 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="zh">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="../../main.py" line="115"/>
         <source>Car ++</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../main.py" line="133"/>
         <source>Camera tab</source>
-        <translation type="unfinished"></translation>
+        <translation>相机页</translation>
     </message>
     <message>
         <location filename="../../main.py" line="134"/>
         <source>Image tab</source>
-        <translation type="unfinished"></translation>
+        <translation>相片页</translation>
     </message>
     <message>
         <location filename="../../main.py" line="135"/>
         <source>Log tab</source>
-        <translation type="unfinished"></translation>
+        <translation>日志页</translation>
     </message>
     <message>
         <location filename="../../main.py" line="144"/>
         <source>Open Camera</source>
-        <translation type="unfinished"></translation>
+        <translation>打开摄像头</translation>
     </message>
     <message>
         <location filename="../../main.py" line="146"/>
         <source>Open camera</source>
-        <translation type="unfinished"></translation>
+        <translation>打开摄像头</translation>
     </message>
     <message>
         <location filename="../../main.py" line="159"/>
         <source>Detect Image</source>
-        <translation type="unfinished"></translation>
+        <translation>检测图像</translation>
     </message>
     <message>
         <location filename="../../main.py" line="161"/>
         <source>Detect image</source>
-        <translation type="unfinished"></translation>
+        <translation>检测图像</translation>
     </message>
     <message>
         <location filename="../../main.py" line="200"/>
         <source>Open a folder to images</source>
-        <translation type="unfinished"></translation>
+        <translation>打开包含图片的文件夹</translation>
     </message>
     <message>
         <location filename="../../main.py" line="201"/>
         <source>Open an image</source>
-        <translation type="unfinished"></translation>
+        <translation>打开图片</translation>
     </message>
 </context>
 </TS>

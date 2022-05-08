bd7d38f383fba08d3d45c29b501c6253f2220ff1
josedelinux <josedelinux@hotmail.com>
2022-05-08 12:32:29 +0800

    Update es.ts

--- translations/es.ts
+++ translations/es.ts
@@ -1,107 +1,107 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="es">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="../main.py" line="403"/>
         <source>Camera tab</source>
-        <translation type="unfinished"></translation>
+        <translation>Pestaña de Cámara</translation>
     </message>
     <message>
         <location filename="../main.py" line="405"/>
         <source>Image tab</source>
-        <translation type="unfinished"></translation>
+        <translation>Pestaña de imagen</translation>
     </message>
     <message>
         <location filename="../main.py" line="407"/>
         <source>Log tab</source>
-        <translation type="unfinished"></translation>
+        <translation>Pestaña de Logging</translation>
     </message>
     <message>
         <location filename="../main.py" line="413"/>
         <source>Open Camera</source>
-        <translation type="unfinished"></translation>
+        <translation>Abrir cámara</translation>
     </message>
     <message>
         <location filename="../main.py" line="417"/>
         <source>Detect Image</source>
-        <translation type="unfinished"></translation>
+        <translation>Detectar imagen</translation>
     </message>
     <message>
         <location filename="../main.py" line="429"/>
         <source>Open &amp;Folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Abrir &amp;Carpeta</translation>
     </message>
     <message>
         <location filename="../main.py" line="207"/>
         <source>Open a folder to images</source>
-        <translation type="unfinished"></translation>
+        <translation>Abrir una carpeta de imágenes</translation>
     </message>
     <message>
         <location filename="../main.py" line="431"/>
         <source>Open &amp;Image</source>
-        <translation type="unfinished"></translation>
+        <translation>Abrir &amp;Imagen</translation>
     </message>
     <message>
         <location filename="../main.py" line="215"/>
         <source>Open an image</source>
-        <translation type="unfinished"></translation>
+        <translation>Abrir una imagen</translation>
     </message>
     <message>
         <location filename="../main.py" line="434"/>
         <source>E&amp;xit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Salir</translation>
     </message>
     <message>
         <location filename="../main.py" line="423"/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Archivo</translation>
     </message>
     <message>
         <location filename="../main.py" line="441"/>
         <source>&amp;Preferences</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Preferencia</translation>
     </message>
     <message>
         <location filename="../main.py" line="438"/>
         <source>&amp;Options</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Opciones</translation>
     </message>
     <message>
         <location filename="../main.py" line="444"/>
         <source>&amp;Language</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Idiomas</translation>
     </message>
     <message>
         <location filename="../main.py" line="451"/>
         <source>&amp;Website</source>
-        <translation type="unfinished"></translation>
+        <translation>Sitio &amp;Web</translation>
     </message>
     <message>
         <location filename="../main.py" line="453"/>
         <source>Show Help (&amp;Markdown)</source>
-        <translation type="unfinished"></translation>
+        <translation>Documento de ayuda(&amp;Markdown)</translation>
     </message>
     <message>
         <location filename="../main.py" line="455"/>
         <source>Show Help (&amp;HTML)</source>
-        <translation type="unfinished"></translation>
+        <translation>Documento de ayuda(&amp;HTML)</translation>
     </message>
     <message>
         <location filename="../main.py" line="458"/>
         <source>&amp;About</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Acerca de</translation>
     </message>
     <message>
         <location filename="../main.py" line="448"/>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Ayuda</translation>
     </message>
     <message>
         <location filename="../main.py" line="462"/>
         <source>Ready</source>
-        <translation type="unfinished"></translation>
+        <translation>Listo</translation>
     </message>
 </context>
 </TS>
